edf0326c2e4223f894140c7bf7c35d5b218d6402
Niklas Schimmack <niklas.schimmack@mni.thm.de>
2023-01-27 16:47:27 +0100

    Updated app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {User} from "./models/user";
+import {DataSource} from "typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: 'root',
+        database: 'digitalFlearMarket',
+        entities: [User],
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
+      User,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {
+  }
+}
