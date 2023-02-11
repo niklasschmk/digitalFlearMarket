78c385610b99612b854a696491fab8809b353e10
sebastian <sebastian.waldschmidt-2@mni.thm.de>
2023-02-11 12:25:33 +0100

    Added providers to auth.module but doesnt work

--- server/src/modules/auth/auth.module.ts
+++ server/src/modules/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import {SellerService} from "../../providers/seller.service/seller.service";
+import {OffererService} from "../../providers/offerer.service/offerer.service";
 
 @Module({
-  providers: [AuthService]
+  providers: [AuthService, SellerService, OffererService]
 })
 export class AuthModule {}
